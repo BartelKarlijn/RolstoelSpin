196d10936813227968f16dfcf4e192132e83af50
BartelKarlijn <legoreus@gmail.com>
2023-01-17 18:34:32 +0100

    blink proberen

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 // Setup
 // =======================================================================================
 void setup()   {
+  pinMode(LED_BUILTIN, OUTPUT);
 //  setupSerial();
   delay(1000);
 //  SetupFinished();
@@ -16,5 +17,8 @@ void setup()   {
 // Loop
 // =======================================================================================
 void loop() {
-
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
 }
