9612ced193f4ae703572c982894e22fce6094e4e
BartelKarlijn <legoreus@gmail.com>
2023-01-17 18:53:26 +0100

    intro blink

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,15 @@
 // =======================================================================================
 void setup()   {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin( SERIALSPEED );
-//  setupSerial();
-  delay(1000);
-//  SetupFinished();
+
+  for (int i=0; i<5; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(200);
+  }
 }
 
 // =======================================================================================
